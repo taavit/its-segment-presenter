e431c9bfd08155bb6090fc7c0ee6d3fe9e577c0f
Dawid Królak <754920+taavit@users.noreply.github.com>
2019-07-22 10:24:31 +0200

    Bumped dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,6 @@ import { ItsApp } from './components/ItsApp';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
-
-injectTapEventPlugin();
-
 // tslint:disable-next-line:variable-name
 const App: React.StatelessComponent = () => (
   <MuiThemeProvider muiTheme={ getMuiTheme() } >
